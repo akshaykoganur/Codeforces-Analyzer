1e7aadd51a13ce78b9d293f4b6f3ed3a673ee6c3
akshaykoganur <akshaykoganur@gmail.com>
2023-12-12 15:59:23 +0530

    commit

--- Project/app/views.py
+++ Project/app/views.py
@@ -45,13 +45,13 @@ def analysis(request):
         total_problems_solved = 0
         flag = 0
         if "rating" in contest_status_data[0]["problem"]:
-            flag = 0
+            fla = 0
         else:
-            continue
+           flag+=1
         for j in range(len(contest_status_data)):
             if(contest_status_data[j]['verdict']=="OK" and past_contest_performance[i][ord(contest_status_data[j]['problem']['index']) - 65]==0):
                 past_contest_performance[i][ord(contest_status_data[j]['problem']['index']) - 65] = 1
-                if(contest_status_data[j]["problem"]["rating"]):
+                if(flag==0 and contest_status_data[j]["problem"]["rating"]):
                     total_problem_rating+=contest_status_data[j]["problem"]["rating"]
                 total_problems_solved+=1
         past_arr[i] = total_problem_rating/total_problems_solved
@@ -61,5 +61,36 @@ def analysis(request):
     fig.add_trace(go.Scatter(y=past_contest_problems_solved, name='Total Problem Solved'), secondary_y=True,)
     chart = fig.to_html
 
-    return render(request, 'analysis.html', context={'user_info': user_data, 'past_contests': past_10_contests, 'past': chart})
+
+    #Past Contests Submission
+    past_contests = []
+    for i in range(len(c_data)):
+        cur_data = c_data[i]
+        past_contests.append(cur_data['contestId'])
+    past_contests_problems_attempted_by_rating = [0]*27
+    past_contests_problems_accepted_by_rating = [0]*27
+    for i in range(len(past_contests)):
+        contest_status_url = 'https://codeforces.com/api/contest.status?contestId='+ str(past_contests[i]) + '&from=1&count=100&handle=' + user_name
+        req2 = requests.get(contest_status_url)
+        data2 = req2.json()
+        contest_status_data = data2['result']
+        flag = 0
+        if "rating" in contest_status_data[0]["problem"]:
+            fla = 0
+        else:
+           flag+=1
+        for j in range(len(contest_status_data)):
+            if(contest_status_data[j]['verdict']=="OK"):
+                if(flag==0 and contest_status_data[j]["problem"]["rating"]):
+                    past_contests_problems_accepted_by_rating[(contest_status_data[j]["problem"]["rating"]-800)//100]+=1
+            if(flag==0):
+                past_contests_problems_attempted_by_rating[(contest_status_data[j]["problem"]["rating"]-800)//100]+=1
+
+    ratings = ['800','900','1000','1100','1200','1300','1400','1500','1600','1700','1800','1900','2000','2100','2200','2300','2400','2500','2600','2700','2800','2900','3000','3100','3200','3300','3400','3500']
+    fig1 = go.Figure()
+    fig1.add_trace(go.Bar(x=ratings,y=past_contests_problems_accepted_by_rating, name='Accepted Submissions in Contest'))
+    fig1.add_trace(go.Bar(x=ratings,y=past_contests_problems_attempted_by_rating, name='Total Submissions in Contest'))
+    chart1 = fig1.to_html
+
+    return render(request, 'analysis.html', context={'user_info': user_data, 'past_c': chart1, 'past': chart})
     #return HttpResponse(req)
